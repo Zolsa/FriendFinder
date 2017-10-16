27758cc3fcdea84c850d92fb2c4904bac501faa5
Zoli <zjsaunt@gmail.com>
2017-10-15 21:59:26 -0400

    finished

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -12,64 +12,56 @@ var friendsData = require("../data/friendsData");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
+  //This route displays the friendsData
   app.get("/friends", function(req, res) {
   	//console.log(friendsData[0]);
     res.json(friendsData);
   });
 
-
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
+  //This route handles when a user sends their data off to the server
   app.post("/friends", function(req, res) {
-
-  	friendsData.push(req.body);
   	var comparisonArray = [];
+    //Find difference between user scores and a friend's and push it to the array
+    var findDiff = function(userScores, compScores) {
+      var compTotal = 0;
+      for(var j = 0; j < 10; j++) {
+        compTotal += Math.abs(compScores[j] - userScores[j]);
+      }
+      comparisonArray.push(compTotal);
+    };
 
-  	//Find the total difference between everyone's stats and the user's (req.scores) that just got posted and push each number to an array
-  	for(var i = 0; i < friendsData.length; i++) {
-  		var compTotal = 0;
-  		for(var j = 0; j < friendsData[0].scores.length; j++) {
-  			compTotal += Math.abs(friendsData[i].scores[j] - req.body.scores[j]);
-  		}
-  		comparisonArray.push(compTotal);
-  	}
+    //Do it for each friend in friendsData
+    for(var i = 0; i < friendsData.length; i++) {
+      findDiff(friendsData[i].scores, req.body.scores);
+    }
 
-  	//Find the index of the smallest number of the array and use that to pick the match 
-  	var index = 0;
-		var value = comparisonArray[0];
-		for (var i = 0; i < comparisonArray.length; i++) {
-		  if (comparisonArray[i] < value) {
-		    index = i;
-		  }
-		}
-  	match = friendsData[index];
+  	//Find the index of the smallest number of the array 
+  	
+    var indexOfSmallest = function(a) {
+      var lowest = 0;
+      for (var i = 1; i < a.length; i++) {
+        if (a[i] < a[lowest]) {
+          lowest = i;
+        }
+      }
+      return lowest;
+    };
 
+    //Use it to identify the match with the least difference
+  	match = friendsData[indexOfSmallest(comparisonArray)];
+    //Add user data to friendsData
+    friendsData.push(req.body);
+    //Return the result for the post call
   	res.json(match);
-
-
   });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  // app.post("/api/clear", function() {
-  //   // Empty out the arrays of data
-  //   friedsDataData = [];
-
-  //   console.log(friendsData);
-  // });
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendsData = [];
+  });
 };
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// Dependencies
-// ===========================================================
+
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -19,47 +18,3 @@ require("./app/routes/htmlRoutes")(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
-
-
-// Get all characters
-// app.get("/all", function(req, res) {
-//   res.json(characters);
-// });
-
-// // Search for Specific Character (or all characters) - provides JSON
-// app.get("/api/:characters?", function(req, res) {
-//   var chosen = req.params.characters;
-
-//   if (chosen) {
-//     console.log(chosen);
-
-//     for (var i = 0; i < characters.length; i++) {
-//       if (chosen === characters[i].routeName) {
-//         return res.json(characters[i]);
-//       }
-//     }
-//     return res.json(false);
-//   }
-//   return res.json(characters);
-// });
-
-// // Create New Characters - takes in JSON input
-// app.post("/api/new", function(req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   var newcharacter = req.body;
-
-//   console.log(newcharacter);
-
-//   // We then add the json the user sent to the character array
-//   characters.push(newcharacter);
-
-//   // We then display the JSON to the users
-//   res.json(newcharacter);
-// });
-
-// // Starts the server to begin listening
-// // =============================================================
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + PORT);
-// });
